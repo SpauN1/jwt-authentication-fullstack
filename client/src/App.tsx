f9527b7bb8d47cf4a9c1cc8ec89fdfc9541c6422
Spaun1 <andreispaun7@gmail.com>
2023-08-18 05:06:04 +0300

    UI react and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Dashboard from './pages/Dashboard/Dashboard';
+
 function App() {
-  return <div className="App">
-    <div>qeqeq</div>
-  </div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Login from '../../components/Login/Login';
+
+function Main() {
+  return (
+    <div>
+      <h1>Main</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default Main;
