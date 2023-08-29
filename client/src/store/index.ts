70adcf57b55c1e174818c266262d4c5a2cfae6c8
Spaun1 <andreispaun7@gmail.com>
2023-08-29 21:31:10 +0300

    add store

---
+++ client/src/store/index.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+import logger from 'redux-logger';
+
+import authReducer from './auth/authReducer';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      ...(process.env.NODE_ENV !== 'production' ? [logger] : [])
+    ),
+});
+
+export type IRootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch; // Export a hook that can be reused to resolve types
