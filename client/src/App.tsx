86408ad5a3fe0ccef5fad60f21d7fa0b4dcb322c
Spaun1 <andreispaun7@gmail.com>
2023-09-19 16:28:42 +0300

    automatic login if there is a live refresh token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,28 @@
-import React, { FC } from 'react';
-
+import { FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import {
+  Navigate,
+  Route,
   BrowserRouter as Router,
   Routes,
-  Route,
-  Navigate,
 } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
-import { IRootState } from './store';
-import Main from './pages/Main/Main';
-import Dashboard from './pages/Dashboard/Dashboard';
 import Header from './components/Header/Header';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Main from './pages/Main/Main';
+import { IRootState, useAppDispatch } from './store';
+import { getProfile } from './store/auth/actionCreators';
 
 const App: FC = () => {
   const isLoggedIn = useSelector(
     (state: IRootState) => !!state.auth.authData.accessToken
   );
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getProfile());
+  }, [dispatch]);
 
   return (
     <Router>
